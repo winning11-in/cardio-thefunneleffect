b55af6460379c5fcb405d5f35d8a59f884c233ff
Ajay <ajay.goswami@coderower.com>
2025-11-21 13:43:24 +0530

    refactor: remove commented code for playlist cards in YouTubePlaylists component

--- components/YouTubePlaylists.tsx
+++ components/YouTubePlaylists.tsx
@@ -52,7 +52,6 @@ const YouTubePlaylists: React.FC = () => {
         </div>
 
          <div className="relative">
-          {/* Playlist Cards */}
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {playlists.slice(0, 6).map((playlist, index) => (
               <div
